021e44d3875459e75811ab7b7b10b55b97b49ba2
daniel99estrada <pildoestrada@gmail.com>
2023-07-18 21:34:30 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useReactMediaRecorder } from 'react-media-recorder';
+import AWS from 'aws-sdk';
 import './App.css';
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
-function App() {
+
+// Update with your AWS S3 bucket details
+const AWS_ACCESS_KEY = 'AKIA5UD2B5HDXF5XQUMR';
+const AWS_SECRET_ACCESS_KEY = 'TTQwITJriRqyhcdxjmMF78gbw4N7uSf0uUL2sSka';
+const AWS_BUCKET_NAME = 'voice-memo-file-storage';
+const AWS_REGION = 'us-east-1'
+
+AWS.config.update({
+  accessKeyId: AWS_ACCESS_KEY,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  region: AWS_REGION,
+});
+
+const s3 = new AWS.S3();
+
+const App = () => {
+  const { status, startRecording, stopRecording, mediaBlobUrl } = useReactMediaRecorder({ audio: true });
+  const [submitted, setSubmitted] = useState(false);
+  const [recordedAudioKey, setRecordedAudioKey] = useState('');
+
+  const handleUpload = () => {
+    if (!mediaBlobUrl) {
+      console.log('No recorded audio found.');
+      return;
+    }
+    
+    const file = new File([mediaBlobUrl], 'recorded-audio.wav', { type: 'audio/wav' });
+
+    s3.putObject().promise()
+
+    const params = {
+      Bucket: AWS_BUCKET_NAME,
+      Key: `recorded-audio-${Date.now()}.wav`,
+      Body: file,
+    };
+    
+
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.error('Failed to upload file:', err);
+      } else {
+        console.log('File uploaded successfully:', data.Location);
+        setRecordedAudioKey(data.Key); // Store the S3 key for the uploaded audio file
+        setSubmitted(true); // Set the submitted state to true
+      }
+    });
+  };
+
+  const handleRefresh = () => {
+    window.location.reload(); // Refresh the page
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>STILLBLUE VOICE MEMOS</h1>
+      {submitted ? (
+        <>
+          <p>Submission Received</p>
+          <button onClick={handleRefresh}>Recording Again</button>
+        </>
+      ) : (
+        <>
+          {status === 'idle' && <button onClick={startRecording}>Start Recording</button>}
+          {status === 'recording' && (
+            <>
+              <p className="recording">RECORDING</p>
+              <button onClick={stopRecording}>Stop Recording</button>
+            </>
+          )}
+          {mediaBlobUrl && (
+          <div className="media-container">
+            <audio src={mediaBlobUrl} controls />
+            <button onClick={handleUpload}>Submit</button>
+        </div>
+          )}
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
