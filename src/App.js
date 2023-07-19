fa4cb8bad2d8db193a72504af8aa356ca14382a3
daniel99estrada <pildoestrada@gmail.com>
2023-07-18 23:07:57 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { useRef } from 'react';
 import { useReactMediaRecorder } from 'react-media-recorder';
 import AWS from 'aws-sdk';
 import './App.css';
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import saveAs from 'file-saver';
 
 
 // Update with your AWS S3 bucket details
@@ -19,38 +21,60 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
+const client = new S3Client({
+  region: AWS_REGION,
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+
 const App = () => {
   const { status, startRecording, stopRecording, mediaBlobUrl } = useReactMediaRecorder({ audio: true });
   const [submitted, setSubmitted] = useState(false);
   const [recordedAudioKey, setRecordedAudioKey] = useState('');
+  const audioRef = useRef();
 
-  const handleUpload = () => {
-    if (!mediaBlobUrl) {
+  const handleUpload = (blobURL) => {
+
+    if (mediaBlobUrl) {
+      const link = document.createElement('a');
+      link.href = mediaBlobUrl;
+      link.download = 'recorded_audio.wav';
+      link.click();
+    }
+
+    if (!blobURL) {
       console.log('No recorded audio found.');
       return;
     }
+  
+    const file = new File([blobURL], 'recorded-audio.mp3', { type: 'audio/mp3' });
+    saveAs(file, 'file/recorded-audio.mp3');
+
+    return new Promise((resolve, reject) => {
+      const params = {
+        Bucket: AWS_BUCKET_NAME, // Replace with your S3 bucket name
+        Key: `recorded-audio-${Date.now()}.wav`,
+        Body: file,
+        ContentType: 'audio/mpeg',
+        ACL: 'public-read'
+      };
     
-    const file = new File([mediaBlobUrl], 'recorded-audio.wav', { type: 'audio/wav' });
+      s3.upload(params, (err, data) => {
+        if (err) {
+          console.error('Failed to upload file:', err);
+        } else {
+          console.log('File uploaded successfully:', data.Location);
+          setRecordedAudioKey(data.Key);
+          setSubmitted(true);
+        }
+      })
 
-    s3.putObject().promise()
-
-    const params = {
-      Bucket: AWS_BUCKET_NAME,
-      Key: `recorded-audio-${Date.now()}.wav`,
-      Body: file,
-    };
-    
-
-    s3.upload(params, (err, data) => {
-      if (err) {
-        console.error('Failed to upload file:', err);
-      } else {
-        console.log('File uploaded successfully:', data.Location);
-        setRecordedAudioKey(data.Key); // Store the S3 key for the uploaded audio file
-        setSubmitted(true); // Set the submitted state to true
-      }
     });
   };
+  
 
   const handleRefresh = () => {
     window.location.reload(); // Refresh the page
